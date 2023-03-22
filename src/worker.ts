202ce87e763217510653fbbe7e269bf9d0c77e15
vylenne <v.vylenne@gmail.com>
2023-03-22 21:30:13 +0400

    add server

--- src/worker.ts
+++ src/worker.ts
@@ -1,5 +1,11 @@
 import { Worker } from 'bullmq';
+import { AppDataSource } from './data-source';
+import { Book } from './entity/Book';
+
 
 const worker = new Worker('books', async book => {
-  console.log(book.data);
+
+  const item = await AppDataSource.manager.create(Book)
+
+  AppDataSource.manager.save(item);
 });
