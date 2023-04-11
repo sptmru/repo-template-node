cd08e947eafc6fdb22a10297089f8655d230602a
vylenne <v.vylenne@gmail.com>
2023-04-11 20:56:20 +0200

    remove unused code

--- src/workers/index.ts
+++ src/workers/index.ts
@@ -12,7 +12,5 @@ interface IBook {
 const worker = new Worker('books', async (book: Job) => {
 
   const bookJob = book.data as IBook
-  const item = await BooksRepository.addBook(bookJob)
-
-  AppDataSource.manager.save(item);
+  const item = await BooksRepository.addBook(bookJob);
 });
